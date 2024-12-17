a07a3450389e95eb30b7ee069cef819efba2a383
ramonskie <ramonmakkelie@gmail.com>
2024-12-17 10:29:10 +0100

    cloudstack uses assets/agent.json

--- stemcell_builder/stages/bosh_cloudstack_agent_settings/apply.sh
+++ stemcell_builder/stages/bosh_cloudstack_agent_settings/apply.sh
@@ -3,32 +3,4 @@
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-agent_settings_file=$chroot/var/vcap/bosh/agent.json
-
-cat > $agent_settings_file <<JSON
-{
-  "Platform": {
-    "Linux": {
-      "PartitionerType": "parted",
-      "CreatePartitionIfNoEphemeralDisk": true,
-      "DevicePathResolutionType": "virtio",
-      "ServiceManager": "systemd"
-    }
-  },
-  "Infrastructure": {
-    "Settings": {
-      "Sources": [
-        {
-          "Type": "HTTP",
-          "URI": "http://169.254.169.254:39724",
-          "UserDataPath": "/latest/user-data",
-          "InstanceIDPath": "/latest/meta-data/instance-id",
-          "SSHKeysPath": "/latest/meta-data/public-keys"
-        }
-      ],
-      "UseServerName": true,
-      "UseRegistry": true
-    }
-  }
-}
-JSON
+cp $assets_dir/agent.json $chroot/var/vcap/bosh/agent.json
